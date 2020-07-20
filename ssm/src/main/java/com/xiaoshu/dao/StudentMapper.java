ec21f83228ad31b2c39c6d1c2b216eec40bc4255
inK18 <3120790136@qq.com>
2020-07-20 14:25:57 +0800

    增删改查

---
+++ ssm/src/main/java/com/xiaoshu/dao/StudentMapper.java
@@ -0,0 +1,30 @@
+package com.xiaoshu.dao;
+
+import com.xiaoshu.base.dao.BaseMapper;
+import com.xiaoshu.entity.School;
+import com.xiaoshu.entity.Student;
+import com.xiaoshu.entity.StudentExample;
+import com.xiaoshu.entity.StudentVo;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface StudentMapper extends BaseMapper<Student> {
+    long countByExample(StudentExample example);
+
+    int deleteByExample(StudentExample example);
+
+    List<Student> selectByExample(StudentExample example);
+
+    int updateByExampleSelective(@Param("record") Student record, @Param("example") StudentExample example);
+
+    int updateByExample(@Param("record") Student record, @Param("example") StudentExample example);
+
+	List<School> findSchool();
+
+	List<StudentVo> findStudentPage(StudentVo studentVo);
+
+	List<Student> selectByName(@Param("studentName")String studentName);
+
+	List<StudentVo> getEcharts();
+}
---
+++ ssm/src/main/java/com/xiaoshu/entity/StudentVo.java
@@ -0,0 +1,25 @@
+package com.xiaoshu.entity;
+
+public class StudentVo extends Student{
+
+
+	private String sname;
+
+	private Integer num;
+	
+	public Integer getNum() {
+		return num;
+	}
+	
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+}
